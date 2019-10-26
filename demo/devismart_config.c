fbf53053cbf557e01d67eafa7cd006366fe0d574
Pavel Fedin <pavel_fedin@mail.ru>
2019-10-26 16:36:19 +0300

    Some fixups and comments

--- demo/devismart_config.c
+++ demo/devismart_config.c
@@ -8,6 +8,13 @@
 #include "mdg_chat_client.h"
 #include "mdg_util.h"
 
+/*
+ * What is going on here is NOT communication with thermostats.
+ * This code handles peer-to-peer communication with the DEVISmart app,
+ * used to share houses. This is the only legitimate way to add a client
+ * to an already running installation.
+ * Communication here is done in JSON and is pretty self-explanatory.
+ */
 static const char *user_name = "DEVIComm test";
 
 static void getMyIdHexed(char *hexed)
@@ -24,7 +31,16 @@ void devismart_request_configuration(uint32_t connection_id)
   Log("Requesting DEVISmart config on connection %d\n", connection_id);
 
   getMyIdHexed(hexed);
-  len = sprintf(json, "{\"phoneName\":\"%s\",\"phonePublicKey\":\"%s\",\"chunkedMessage\":true}", user_name, hexed);
+  /*
+   * If chunkedMessage parameter is set to true, the whole data will be split
+   * into 512-byte long chunks and sent as separate packets; we would have
+   * to put them together during receiving; we don't want to do that.
+   * In this case a header is sent in the first packet, describing total
+   * length of the configuration data. See struct ConfigDataHeader.
+   * As of DEVISmart v1.2 this parameter is optional and can be completely
+   * omitted; default value is false.
+   */
+  len = sprintf(json, "{\"phoneName\":\"%s\",\"phonePublicKey\":\"%s\",\"chunkedMessage\":false}", user_name, hexed);
   
   Log("Sending request: %s\n", json);
   s = mdg_send_to_peer((uint8_t *)json, len, connection_id);
@@ -35,7 +51,7 @@ void devismart_request_configuration(uint32_t connection_id)
 
 struct ConfigDataHeader
 {
-	uint32_t start;  // Always 0
+	uint32_t start;  // Always 0, used for presence detection
 	uint32_t length; // Total length of the following data
 };
 
@@ -47,7 +63,7 @@ uint32_t devismart_receive_config_data(const uint8_t *data, uint32_t size)
   
     if (header->start == 0)
     {
-	  Log("Full data size: %d\n", header->length);
+	  Log("Full size of chunked data: %d\n", header->length);
 	  data += sizeof(struct ConfigDataHeader);
 	  size -= sizeof(struct ConfigDataHeader);
 	}
@@ -59,6 +75,11 @@ uint32_t devismart_receive_config_data(const uint8_t *data, uint32_t size)
   buf[size] = 0;
 
   Log("Received configuration data:\n%s\n", buf);  
-  
+
+/*
+ * TODO: Parse this JSON and add peers
+  {"houseName":"My Flat","houseEditUsers":true,"rooms":[{"roomName":"Гостиная","peerId":"<undisclosed>","zone":"Living","sortOrder":0}]}
+*/
+
   return 0;
 }
