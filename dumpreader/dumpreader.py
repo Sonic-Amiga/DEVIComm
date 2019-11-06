f4227daaf3d39369025ec3ea345dd1ab97be6b21
Pavel Fedin <pavel_fedin@mail.ru>
2019-11-06 23:24:16 +0300

    Understood binary serialization
    Figured out header and data offsets. Now the decoder successfully reads all the
    packets.

--- dumpreader/dumpreader.py
+++ dumpreader/dumpreader.py
@@ -7,12 +7,23 @@ import sys
 # had to rename parse.py, otherwise it conflicted with the standard parse module
 import protobuf
 
-def DumpData(data, offset):    
-    size = len(data) / 2
+def DumpData(data, offset, decodeHeader):
+    size = len(data)
+    if size > 128 and data[128:143] == "\x01\x0bcertificate\x00\x80":
+        # VOCH payload appears to be not protobuf but something else.
+        print "Skipping VOCH payload, not a protobuf"
+        return
     if size > offset:
-        binary = binascii.unhexlify(data)
+        if decodeHeader:
+            if size < offset + 3:
+                print "Packet is too short, no header"
+                return
+            msgsize = (ord(data[offset]) << 8) | ord(data[offset + 1])
+            msgtype = ord(data[offset + 2])
+            print "Data length " + str(msgsize) + " type " + str(msgtype)
+            offset = offset + 3
         messages = {}
-        protobuf.ParseData(binary, offset, size - offset, messages)
+        protobuf.ParseData(data, offset, size, messages)
         print json.dumps(messages, indent=4, sort_keys=True, ensure_ascii=False, encoding='utf-8')
 
 if len(sys.argv) < 2:
@@ -23,21 +34,23 @@ infile = open(sys.argv[1], "r")
 data = ""
 
 if len(sys.argv) > 2:
-    offset = sys.argv[2]
+    offset = int(sys.argv[2])
+    decodeHeader = False
 else:
-    # We assume that the first packet is VOCH payload, and it has 128 bytes of inner
-    # crypto box, containing the key. Skip it over.
-    offset = 128
+    # Every packet has 32 bytes of padding plus 3 byte header:
+    # 2 bytes - length of the following protobuf payload, bigendian
+    # 1 byte - structure type
+    offset = 32
+    decodeHeader = True
 
 for line in infile:
     line = line.strip()
     if len(line) > 48 and line[0] in string.hexdigits and line[1] in string.hexdigits and line[2] == " ":
-        data = data + line[:48].replace(" ", "")
+        data = data + binascii.unhexlify(line[:48].replace(" ", ""))
     else:
-        DumpData(data, offset)
-        print line
+        DumpData(data, offset, decodeHeader)
         data = ""
-        offset = 32 # Every packet has 32 bytes of padding
-DumpData(data, offset)
+        print line
+DumpData(data, offset, decodeHeader)
 infile.close()
 
